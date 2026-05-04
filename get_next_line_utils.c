6f8c0cfe2f5b37ad94c097712e3040da1467d8ed
Nelson Da Rocha Dias <nda-roch@c1r10s7.42porto.com>
2026-05-04 19:08:49 +0100

    added stuff

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,98 @@
 /*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 20:26:10 by nda-roch          #+#    #+#             */
-/*   Updated: 2026/05/01 20:26:12 by nda-roch         ###   ########.fr       */
+/*   Updated: 2026/05/04 19:08:23 by nda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_strlen(char *s)
+{
+	int	i;
 
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	int		s1_size;
+	int		s2_size;
+	char	*result;
+
+	i = 0;
+	j = 0;
+	s1_size = ft_strlen(s1);
+	s2_size = ft_strlen(s2);
+	result = malloc(s1_size + s2_size + 1);
+	if (!result)
+		return (NULL);
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		result[i + j] = s2[j];
+		j++;
+	}
+	result[i + j] = '\0';
+	return (result);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char) c)
+			return ((char *) s);
+		s++;
+	}
+	if ((char) c == '\0')
+		return ((char *) s);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*result;
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		result = malloc(1);
+		result[0] = '\0';
+		return (result);
+	}
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	result = malloc(len + 1);
+	if (!result)
+		return (NULL);
+	while (s[start + i] && i < len)
+	{
+		result[i] = s[start + i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_first_line(char *stash)
+{
+	char	*first_line;
+
+	if (!stash)
+		return (NULL);
+	next_line = ft_substr(stash, 0, ft_strchr(stash, '\n') - stash + 1);
+	return (next_line);
+}
